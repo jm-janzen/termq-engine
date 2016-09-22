17eadb1d280d5db590ebb34cc45a00a28fda8048
JM <jmjanzen@gmail.com>
2016-09-22 18:31:15 -0400

    Churning fixes to game header.
    * Remove pointless typedefs (these are named structs, after all).
    * Convert unsigned fast ints to all same type for now.
    * Move struct names next to their datatype.
    * Add offset to const game_area, so I don't have to fiddle as
      much with these numbers at runtime.

--- src/game.h
+++ src/game.h
@@ -3,37 +3,42 @@
 
 #include <stdint.h> // Use <inttype.h> if portability important
 
-typedef struct {
+struct vec2ui {
     uint_fast8_t x;
     uint_fast8_t y;
-} vec2ui;
+};
 
-typedef struct {
+struct vec2i {
     int_fast8_t x;
     int_fast8_t y;
-} vec2i;
 
-const typedef struct {
+    bool operator==(const vec2i &a) const {
+        return (a.x == x && a.y == y);
+    }
+};
+
+struct rect {
     vec2i offset;
     vec2i bounds;
 
-    uint_fast16_t top()     const { return offset.y; }
-    uint_fast16_t bot()     const { return offset.y + bounds.y; }
-    uint_fast16_t left()    const { return offset.x; }
-    uint_fast16_t right()   const { return offset.x + bounds.x; }
+    int_fast8_t top()     const { return offset.y; }
+    int_fast8_t bot()     const { return height() - offset.y ; }
+    int_fast8_t left()    const { return offset.x; }
 
-    uint_fast16_t width()   const { return bounds.x; }
-    uint_fast16_t height()  const { return bounds.y; }
+    int_fast8_t right()   const { return width() - offset.x; }
+
+    int_fast8_t width()   const { return bounds.x; }
+    int_fast8_t height()  const { return bounds.y; }
 
     bool contains(vec2i a)  const {
         return (a.x >= offset.x && a.x < right())
             && (a.y >= offset.y && a.y < bot());
     }
 
-} rect;
+};
 
 const rect game_area = {
-    {0, 0},
+    {2, 2},
     {80, 40}
 };
 
