fb3e23ab13cf5e2d8a80312f0b25433bbf325e76
JM <jmjanzen@gmail.com>
2016-10-03 13:11:34 -0400

    [#37] Adds tiny and big coins
    While here, increase Item max value to 16 bit.

--- src/classes/Coin.cpp
+++ src/classes/Coin.cpp
@@ -6,8 +6,20 @@
 #include "../game.h"
 
 Coin::Coin() {
-    setChar('o');
-    setValue(100);
+    switch(rand() % 3) {
+        case 0:
+            setChar('.');
+            setValue(25);
+            break;
+        case 1:
+            setChar('o');
+            setValue(100);
+            break;
+        case 2:
+            setChar('O');
+            setValue(250);
+            break;
+    }
 
     uint_fast8_t randx = (rand() % (game_area.right() -1) + 1);
     uint_fast8_t randy = (rand() % (game_area.bot()   -1) + 1);
