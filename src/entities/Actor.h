7d90adba95edd5ca809b2ec5dc6e4d8307a276c5
JM <jmjanzen@gmail.com>
2016-10-08 11:27:05 -0400

    Actor, Item: Inherit from new Parent class Entity
    Entity contains methods common to both Actor & Item (display, position).

--- src/entities/Actor.h
+++ src/entities/Actor.h
@@ -8,23 +8,16 @@
 
 #include "../directions.h"
 #include "../game.h"
-
 #include "../windows/Window.h"
+#include "Entity.h"
 
-class Actor {
+class Actor : public Entity {
     public:
 
         Actor() {};
         Actor(Window *newW);
 
-        void render();
-
-        bool atop(vec2ui const targetPos);
-        bool isAdjacent(vec2ui const targetPos);
-        int_fast8_t getDistance(vec2ui const targetPos);
-        int_fast8_t getDistanceX(vec2ui const targetPos);
-        int_fast8_t getDistanceY(vec2ui const targetPos);
-
+        // Movement
         void  moveNorth();
         void  moveNorthEast();
         void  moveEast();
@@ -35,45 +28,18 @@ class Actor {
         void  moveNorthWest();
 
 
-        // Check cardinal direction
-        bool  isNorth(Actor &target);
-        bool  isSouth(Actor &target);
-        bool  isEast(Actor &target);
-        bool  isWest(Actor &target);
-
-        // Returns ranges between cardinal directions
-        bool  isNorthEast(Actor &target);
-        bool  isSouthEast(Actor &target);
-        bool  isSouthWest(Actor &target);
-        bool  isNorthWest(Actor &target);
-
-        Direction getDirection(Actor &a);  // Get ray from self to target
-
+        // Iterators
         void  wait();
         void  move();
         void  tick();
         void  step();
 
-        void  setPosRand();
-        void  setPos(vec2ui newPos);
-        void  setChar(char newChar);
-        void  setColo(chtype newColo);
-
-        vec2ui getPos();
-        char  getDispChar();
-        int   getDispColo();
         int   getSteps() { return steps; }
         int   getTicks() { return ticks; }
 
     protected:
-        vec2ui pos;
-        char  disp_char;
-        int   disp_colo;
         int   steps = 0;
         int   ticks = 0;
-        Window *w;
-
-        Direction direction = H;
 };
 
 #endif
