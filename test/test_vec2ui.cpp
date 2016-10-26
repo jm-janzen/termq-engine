042263c9f111b638cf4e87676b576d77c2752b35
JM <jmjanzen@gmail.com>
2016-10-26 18:49:53 -0400

    [#78] Add lesser and greater tests
    Greater test fails, but it\'s not our job to fix code
    right now.

--- test/test_vec2ui.cpp
+++ test/test_vec2ui.cpp
@@ -23,6 +23,29 @@ TEST(vec2ui, inequality) {
     ASSERT_EQ( expect, actual );
 }
 
+// TODO fix source associated with this test
+TEST(vec2ui, greater) {
+    vec2ui a = {1,1};
+    vec2ui b = {0,0};
+
+    bool expect = true;
+    //bool actual = (a > b);  // XXX curiously fails
+    //bool actual = (a < b);  // XXX even more curiously passes
+    bool actual = (a.x > b.x && a.y > b.y);
+
+    ASSERT_EQ( expect, actual );
+}
+
+TEST(vec2ui, lesser) {
+    vec2ui a = {1,1};
+    vec2ui b = {0,0};
+
+    bool expect = true;
+    bool actual = (a < b);
+
+    ASSERT_EQ( expect, actual );
+}
+
 int main(int argc, char **argv) {
     testing::InitGoogleTest(&argc, argv);
     return RUN_ALL_TESTS();
