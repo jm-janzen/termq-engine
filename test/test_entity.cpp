1236604ac0af7c4a2c3e15d1ded536fc2e273a15
JM <jmjanzen@gmail.com>
2016-10-29 13:58:43 -0400

    [#78] 100% test method coverage for Entity class
    Although not quite 100% code coverage (conditional branches).  This
    is an enhancement left for another sprint.

--- test/test_entity.cpp
+++ test/test_entity.cpp
@@ -30,7 +30,8 @@ TEST(Entity, isDirection) {
     e2.setPos({0,0}); // target
 
     // target is NorthWest of source
-    bool expect = (
+    bool expect = true;
+    bool actual = (
            !e1.isNorth(e2)
         && !e1.isSouth(e2)
         && !e1.isEast(e2)
@@ -40,9 +41,74 @@ TEST(Entity, isDirection) {
         && !e1.isSouthWest(e2)
         &&  e1.isNorthWest(e2)
     );
-    bool actual = true;
 
     ASSERT_EQ( expect, actual );
+}
+
+TEST(Entity, atop) {
+    Entity e;
+    e.setPos({0,0});
+    vec2ui nextToEntity = {1,1};
+
+    bool expect = true;
+    bool actual = (e.isAdjacent(nextToEntity));
+
+    ASSERT_EQ( expect, actual );
+}
+
+TEST(Entity, isAdjacent) {
+    Entity e;
+    e.setPos({0,0});
+    vec2ui onEntity = {0,0};
+
+    bool expect = true;
+    bool actual = (e.atop(onEntity));
+
+    ASSERT_EQ( expect, actual );
+}
+
+TEST(Entity, getDistance) {
+    Entity e;
+    e.setPos({0,0});
+    vec2ui nearBy = {10,10};
+
+    int_fast8_t expect = 14;  // square root of getDistanceX, getDistanceY
+    int_fast8_t actual = e.getDistance(nearBy);
+
+    ASSERT_EQ( expect, actual );
+}
+
+TEST(Entity, set_getPosRand) {
+    Entity e;
+    e.setPos({0,0});
+    e.setPosRand();
+
+    // Position must be within bounds of game_area rect
+    bool expect = true;
+    bool actual = (
+           (e.getPos().x < game_area.right() && e.getPos().x > game_area.left())
+        && (e.getPos().y < game_area.bot()   && e.getPos().y > game_area.top())
+    );
+
+    ASSERT_EQ( expect, actual );
+}
+
+TEST(Entity, getDirection) {
+    Entity e1;
+    Entity e2;
+    e1.setPos({0,0});
+    e2.setPos({5,0});
+
+    Direction expect = E;  // Target is East of source
+    Direction actual = e1.getDirection(e2);
+
+    ASSERT_EQ( expect, actual );
+}
+
+TEST(Entity, render) {
+    Entity e;
+    Window w({{0,0},{10,10}});  // 10x10 Window in top-left position
+    e.render(w);  // Void function - deprecated anyway
 }
 
 /*
